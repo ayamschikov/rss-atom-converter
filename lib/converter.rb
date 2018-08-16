b70e559e94fa5d3f75766ca2adc849e7534e110f
Alexandr Yamschikov <yamshchikov.as73@gmail.com>
2018-08-16 18:02:28 +0400

    Add converter methods parse and reverse

--- lib/converter.rb
+++ lib/converter.rb
@@ -1,18 +1,21 @@
 class Converter
-  attr_reader :result
-  attr_accessor :rss
-  def initialize(file, options)
+  def initialize(hash, options)
+    @data = hash
     @options = options
+    self.sort
+    self.reverse
+    return @data
   end
 
-  # TODO: check if link or file is available
-  # send error if not
-  def read
-    if @file =~ /http/
-      @rss = RSS::Parser.parse(open(@file))
-    else
-      @rss = RSS::Parser.parse(File.read(@file))
+  def reverse
+    if @options['reverse']
+      @data.items.reverse!
     end
   end
 
+  def sort
+    if @options['sort']
+      @data.items.sort! {|a, b| a.pubDate <=> b.pubDate}
+    end
+  end
 end
