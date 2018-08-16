480c2db4e47b01f7f599fef5d9619280dfd9d734
Alexandr Yamschikov <yamshchikov.as73@gmail.com>
2018-08-16 13:35:21 +0400

    Add Converter and CommandParser classes

--- converter.rb
+++ converter.rb
@@ -4,56 +4,91 @@ require 'open-uri'
 require 'rss'
 require 'optparse'
 
-options = {}
-OptionParser.new do |parser|
-  parser.banner = "Usage: converter.rb [options] FILE"
-
-  parser.on("-h", "--help", "Show this help message") do ||
-    puts parser
-    exit
+class Converter
+  attr_reader :result
+  attr_accessor :rss
+  def initialize(file, options)
+    @file = file
+    @options = options
   end
 
-  parser.on("-o", "--out TYPE", "Output type [rss | atom]") do |v|
-    if v == 'atom'
-      options[:output] = v
+  # TODO: check if link or file is available
+  # send error if not
+  def read
+    if @file =~ /http/
+      @rss = RSS::Parser.parse(open(@file))
     else
-      options[:output] = 'rss'
+      @rss = RSS::Parser.parse(File.read(@file))
     end
   end
 
-  parser.on("-r", "--reverse", "Reverse items") do ||
-    options[:reverse] = true
+  # TODO: add atom support
+  def reverse
+    if @options['reverse']
+
+      @rss.channel.items.reverse!
+    end
   end
 
-  parser.on("-s", "--sort", "Sort by publishing date") do ||
-    options[:sort] = true
+  def sort
+    if @options['sort']
+      @rss.channel.items.sort! {|a, b| a.pubDate <=> b.pubDate}
+    end
   end
 
-end.parse!
-
-file = ARGV[0]
-
-# TODO: check if link or file is available
-# send error if not
-if file =~ /http/
-  rss = RSS::Parser.parse(open(file))
-else
-  rss = RSS::Parser.parse(File.read(file))
+  def convert
+    self.read
+    self.reverse
+    self.sort
+    # TODO: implement rss-atom conversion
+    if !@options['output']
+      @options['output'] = 'RSS20'
+    end
+    return @rss
+    #result = RSS::Maker.make(options[:output]) { |maker| rss}
+  end
 end
 
-# TODO: add atom support
-if options[:reverse]
-  rss.channel.items.reverse!
+class CommandParser
+  def initialize
+    @options = {}
+  end
+
+  def parse
+    OptionParser.new do |parser|
+      parser.banner = "Usage: converter.rb [@options] FILE"
+
+      parser.on("-h", "--help", "Show this help message") do ||
+        puts parser
+        exit
+      end
+
+      parser.on("-o", "--out TYPE", "Output type [rss | atom]") do |v|
+        if v == 'atom'
+          @options['output'] = v
+        else
+          @options['output'] = 'rss'
+        end
+      end
+
+      parser.on("-r", "--reverse", "Reverse items") do ||
+        @options['reverse'] = true
+      end
+
+      parser.on("-s", "--sort", "Sort by publishing date") do ||
+        @options['sort'] = true
+      end
+
+    end.parse!
+
+    file = ARGV[0]
+    return file, @options
+  end
 end
 
-if options[:sort]
-  rss.channel.items.sort! {|a, b| a.pubDate <=> b.pubDate}
-end
-
-# TODO: implement rss-atom conversion
-if !options[:output]
-  options[:output] = 'RSS20'
-end
-#result = RSS::Maker.make(options[:output]) { |maker| rss}
+cParser = CommandParser.new
+file, options = cParser.parse
+converter = Converter.new(file, options)
+rss = converter.convert
 
 STDOUT.puts(rss)
