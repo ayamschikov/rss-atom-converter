ac187ba0cd35ec5663855b0047807fd27ace84b3
Alexandr Yamschikov <yamshchikov.as73@gmail.com>
2018-08-21 12:29:53 +0400

    Correct convertation classes

--- lib/converter/atom_converter.rb
+++ lib/converter/atom_converter.rb
@@ -7,20 +7,15 @@ class AtomConverter
       @result = RSS::Maker.make("atom") do |maker|
         # Required
         maker.channel.updated = Time.now
-        # hash.title doesn't work
-        maker.channel.title = 'test title' #hash.title
-        # hash.id doesn't work
-        maker.channel.id = 'test id' #hash.id
-
+        maker.channel.title = hash[:title]
+        maker.channel.id = @@default_fields[:id]
         maker.channel.author = @@default_fields[:author]
-
 
         hash[:items].each do |item|
           maker.items.new_item do |entry|
             entry.link = item[:link]
             entry.title = item[:title]
-            entry.published = item[:pubDate]
-            entry.updated = item[:pubDate]
+            entry.updated = item[:published]
           end
         end
       end
--- lib/converter/rss_converter.rb
+++ lib/converter/rss_converter.rb
@@ -1,33 +1,23 @@
 class RssConverter
   @@default_fields = {
-    description: 'default_description'
+    description: 'default_description',
+    link: 'default_link'
   }
 
   def self.to_rss(hash)
-
       @result = RSS::Maker.make("2.0") do |maker|
         #  Required
-        #  Check for existing these fields
         maker.channel.title = hash[:title]
-        maker.channel.link = hash[:link]
-        # doesn't work, fix this
-        # maker.channel.description = hash.any?(&:subtitle?) ? '' : hash.subtitle
+        maker.channel.link = @@default_fields[:link]
         maker.channel.description = @@default_fields[:description]
-
-        # Optional
-        #  maker.channel.author = hash.author.name.content
-        #  maker.channel.updated = hash.updated.content
-        #  maker.channel.id = hash.id.content
 
         hash[:items].each do |entry|
           maker.items.new_item do |item|
             item.link = entry[:link]
             item.title = entry[:title]
-            item.updated = entry[:updated]
             item.pubDate = entry[:published]
           end
         end
       end
-
   end
 end
