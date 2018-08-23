27817db6a71fa11dc85c70b667a4eb582ee23c37
Alexandr Yamschikov <yamshchikov.as73@gmail.com>
2018-08-23 16:47:50 +0400

    Add rss parser test

---
+++ test/parser/rss_parser_test.rb
@@ -0,0 +1,14 @@
+require 'require_all'
+require './test/test_helper.rb'
+require 'rss'
+require_rel '../../lib'
+
+class RssParserTest < Minitest::Test
+  def test_parse
+    source = ParseHelper.parse(File.read('test/fixtures/hexlet.rss'))
+    rss_parsed_file = File.read('test/fixtures/rss_parsed_hexlet')
+
+    assert rss_parsed_file == source.to_s
+  end
+
+end
