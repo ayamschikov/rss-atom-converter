27c1edae4594169fc41ec244580997004cd309ed
Alexandr Yamschikov <yamshchikov.as73@gmail.com>
2018-08-23 16:20:29 +0400

    Fix sorthandler

--- lib/handler/sort_handler.rb
+++ lib/handler/sort_handler.rb
@@ -4,7 +4,7 @@ class SortHandler
   end
 
   def process(source)
-    @options.each { |sort_type| source.sort! { |a, b| a[sort_type] <=> b[sort_type] } }
+    @options.each { |sort_type| source.sort! { |a, b| a[sort_type.to_sym] <=> b[sort_type.to_sym] } }
     source
   end
 end
